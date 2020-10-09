f9e8d220f143d0ad05d10c6dbd573142a817bafc
Malachi Spencer <m.spencer@live.co.uk>
2020-10-09 15:06:02 +0200

    Added code to pass first test

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,8 @@
+
+class Numeric
+  def fizzbuzz
+    if self % 3 == 0
+      'Fizz'
+    end
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,9 @@
+#require 'fizzbuzz'
+require './lib/fizzbuzz'
+#require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns fizz when called on 3' do
-    expect(3.fizzbuzz).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'Fizz'
   end
 end
